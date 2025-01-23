8b743ff8ced71360b4d17c4344eab880142ad3ef
PikkiKaju <igor.zmuda02@gmail.com>
2025-01-23 12:07:39 +0100

    - updated the functionality of PredictFile - added command line args and changes the output shape - added a trained model - Changed .gitignore

--- backend/HDHGN/PredictFile.py
+++ backend/HDHGN/PredictFile.py
@@ -1,21 +1,21 @@
 import argparse
 import torch
 import ast
+from colorama import Fore, Style
 
-from models.HDHGN import HDHGN
 from MyDataset import HDHGData
-from utilities.vocab import Vocab
+from vocab import Vocab
 from utilities.utils import pre_walk_tree
 
 
 # Initialize argument parser
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(prog="PredictFile", description="Predict the label for a given file using a pre-trained model.")
 
 # Adding optional arguments
+parser.add_argument("-fp", "--file_path", help = "Path to the file to be predicted", type = str, required=True)
 parser.add_argument("-mp", "--model_path", help = "Path to the pre-trained model", type = str, default = "work_dir/HDHGN/HDHGN.pt")
 parser.add_argument("-vp", "--vocab_path", help = "Path to the vocabulary file", type = str, default = "data/vocab4ast.json")
-parser.add_argument("-fp", "--file_path", help = "Path to the file to be predicted", type = str, default = "data/additional/file_to_test.py")
-
+parser.add_argument("-s", "--show_output", help = "Whether to show the probabilities for each label or not", type=bool, action = argparse.BooleanOptionalAction, default = False)
 
 # Read arguments from command line
 args = parser.parse_args()
@@ -31,6 +31,8 @@ def predict(file_path: str, model_path: str, vocab_path: str):
         vocab_path (str): Path to the vocabulary file.
 
     Returns:
+        torch.Tensor: Predicted output tensor.
+        [(str, float, float)]: List of tuples containing the label, similarity value, and probability.
         str: Predicted label for the file.
     """
     # Load vocab
@@ -69,21 +71,45 @@ def predict(file_path: str, model_path: str, vocab_path: str):
     # Make prediction
     with torch.no_grad():
         output = model(data.x, data.types, data.edge_types, data.edge_in_out_indexs, data.edge_in_out_head_tail, data.batch)
-        print(f"Model output: {output}")
-        prediction = torch.argmax(output, dim=-1)
-        print(f"Prediction index: {prediction}")
-    
-    # Decode prediction
-    label = vocab.vocab["labels"].id2word[prediction.item()]
-    probabilities = torch.nn.functional.softmax(output, dim=-1)
-    return output, probabilities, label
+        # probabilities = torch.nn.functional.softmax(output, dim=-1)
+        probabilities = torch.nn.functional.softmax(output)
+
+    # Decode predictions
+    labels = list(vocab.vocab["labels"].word2id.keys())
+
+    sorted_indices = torch.argsort(output, descending=True)
+    sorted_labels = [labels[i] for i in sorted_indices[0]]
+    sorted_values = [output[0][i].item() for i in sorted_indices[0]]
+    sorted_probabilities = torch.sort(probabilities, descending=True).values
+
+
+    # Create output frame
+    output_frame = []
+    for label, value, probability in zip(sorted_labels, sorted_values, sorted_probabilities[0]):
+        output_frame.append((label, value, probability.item()))
+
+    sorted_values_max = 0
+    for value in sorted_values:
+        if value > 0:
+            sorted_values_max += value
+    sorted_probabilities = (value / sorted_values_max for value in sorted_values)
+    output_frame = []
+    for label, value, probability in zip(sorted_labels, sorted_values, sorted_probabilities):
+        output_frame.append((label, value, probability))
+
+    return output_frame
+
 
 if __name__ == '__main__':
+    print(Fore.GREEN + "Making predictions..." + Style.RESET_ALL, flush=True)
     model_path = args.model_path
     vocab_path = args.vocab_path
     file_to_test = args.file_path
-    print("Making predictions...")
-    output, probabilities, label = predict(file_to_test, model_path, vocab_path)
-    print(f"The output for the file is: {output}")
-    print(f"The probabilities for each label are: {probabilities}")
+
+    output_frame = predict(file_to_test, model_path, vocab_path)
+    label = output_frame[0][0]
     print(f"The predicted label for the file is: {label}")
+    if args.show_output:
+        print("\nSorted labels with probability values:")
+        for label, value, probability in output_frame:
+            print(f"{label:<30} {value:.2f}  {probability:.4f}")
